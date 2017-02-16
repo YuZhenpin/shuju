2ba3ba85b2444147da376616b326a9e7c4e89bee
YuZhenpin <yuzhenpin@126.com>
2017-02-16 15:07:34 +0800

    Update env.mk

--- env.mk
+++ env.mk
@@ -1,25 +1,6 @@
-distillery_base_domain: rnd-ideaproject.huawei.com
-#distillery_base_domain: project.hao.com
+SSH_USERNAME    = root
+SSH_PRIVATE_KEY = ~/.ssh/id_rsa 
 
-proxy_peer: "true"
-proxy_peer_address: "qeesung.host.huawei.com"
-proxy_peer_port: 3128
-
-#proxy_peer_address: "proxyde.huawei.com"
-#proxy_peer_port: 8080
-#proxy_peer_username: "h00283784"
-#proxy_peer_password: "!jiki123"
-#proxy_internal_network: 'all'
-
-longrow_retention_days: 31
-
-###
-# DNS forwarder configuration
-###
-# this value does not need to be set, then bind default recursors to root level DNS servers
-# (so domains from company internal DNS servers are not seen.). A list internal (forwarding)
-# DNS servers can be given here. Typically the entries from `/etc/resolv.conf` modulus `127.0.0.1`
-#
-bind_forwarders:
-  - 10.72.255.100
-  - 10.72.55.82
+DEPLOY_LOG_SERVER = yes
+# make ansible configure resolve.conf on dnsmasq startup
+ANSIBLE_ENV_ARGS = -e dnsmasq_set_resolv_conf_by=systemd
